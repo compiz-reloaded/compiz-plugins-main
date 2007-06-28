794de753e620fc452ed4f1c38cb3074f071b6356
Danny Baumann <dannybaumann@web.de>
2007-06-28 16:53:11 +0200

    Fix some possible NULL pointer dereferences. Thanks to Roland Bär for catching those.

---
+++ src/animation/dream.h
@@ -0,0 +1,7 @@
+void
+fxDreamModelStepObject(CompWindow * w,
+					   Model * model, Object * object, float forwardProgress);
+void fxDreamModelStep(CompScreen * s, CompWindow * w, float time);
+void
+fxDreamUpdateWindowAttrib(AnimScreen * as,
+						  AnimWindow * aw, WindowPaintAttrib * wAttrib);
