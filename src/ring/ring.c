3a777fd316b326c9e27976b4e41f678458087504
Guillaume Seguin <guillaume@segu.in>
2007-07-18 16:09:13 +0200

    * Fix typo

--- src/ring/ring.c
+++ src/ring/ring.c
@@ -1796,7 +1796,11 @@ CompPluginVTable ringVTable = {
     0,
     0,
     0,
-    0
+    0,
+    0, /* Deps */
+    0, /* nDeps */
+    0, /* Features */
+    0  /* nFeatures */
 };
 
 CompPluginVTable *
