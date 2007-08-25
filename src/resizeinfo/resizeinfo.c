a106ce95d7ee45c756beef0bfe39e95557b56365
Danny Baumann <dannybaumann@web.de>
2007-08-25 18:28:00 +0200

    Adjust header.

--- src/resizeinfo/resizeinfo.c
+++ src/resizeinfo/resizeinfo.c
@@ -6,6 +6,9 @@
  *
  * Copyright (c) 2007 Robert Carr <racarr@opencompositing.org>
  *
+ * Compiz resize atom usage and general cleanups by
+ * Copyright (c) 2007 Danny Baumann <maniac@opencompositing.org>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
