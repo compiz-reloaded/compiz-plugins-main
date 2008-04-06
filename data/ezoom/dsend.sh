29ff5c011050e02f75bef83a0aac4403ee73c4ae
Dennis Kasprzyk <onestone@opencompositing.org>
2008-04-06 05:10:23 +0200

    Cleanup.

--- data/ezoom/dsend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-# Sends a dbus option to a plugin
-#
-dbus-send --type=method_call --dest=org.freedesktop.compiz /org/freedesktop/compiz/$1/allscreens/$2 org.freedesktop.compiz.activate string:'root' int32:`xwininfo -root | grep id: | awk '{ print $4 }'` $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14}
-echo dbus-send --type=method_call --dest=org.freedesktop.compiz /org/freedesktop/compiz/$1/allscreens/$2 org.freedesktop.compiz.activate string:'root' int32:`xwininfo -root | grep id: | awk '{ print $4 }'` $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14}
