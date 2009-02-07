88400f3fbff057b8b95e5e4478f9f9605ef7f7f5
Kristian Lyngstol <kristian@bohemians.org>
2009-02-07 17:34:39 +0100

    Fix warning

--- src/text/text.c
+++ src/text/text.c
@@ -662,7 +662,7 @@ textGetObjectOptions (CompPlugin *plugin,
     };
 
     RETURN_DISPATCH (object, dispTab, ARRAY_SIZE (dispTab),
-		     (void *) (*count = 0), (plugin, object, count));
+		     0, (plugin, object, count));
 }
 
 
