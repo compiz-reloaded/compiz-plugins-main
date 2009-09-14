12f054183bb87dc2d2a53df8728825e967ed577d
Erkin Bahceci <erkinbah@gmail.com>
2009-09-14 00:17:18 -0500

    staticswitcher: Prevent simultaneous plugin uses that don't make sense.

--- src/staticswitcher/staticswitcher.c
+++ src/staticswitcher/staticswitcher.c
@@ -590,7 +590,7 @@ switchInitiate (CompScreen            *s,
 
     SWITCH_SCREEN (s);
 
-    if (otherScreenGrabExist (s, "switcher", "scale", "cube", NULL))
+    if (otherScreenGrabExist (s, "cube", NULL))
 	return;
 
     ss->selection      = selection;
