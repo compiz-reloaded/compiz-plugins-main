06e5201f3a6849515ad4df603bdab76625fa4d3c
Scott Moreau <oreaus@gmail.com>
2016-09-12 19:15:13 -0600

    shift: Fix compiler warnings

--- src/shift/shift.c
+++ src/shift/shift.c
@@ -344,13 +344,12 @@ shiftRenderWindowTitle (CompScreen *s)
 static void
 shiftDrawWindowTitle (CompScreen *s)
 {
-    float width, height, border = 10.0f;
+    float height, border = 10.0f;
     int ox1, ox2, oy1, oy2;
 
     SHIFT_SCREEN (s);
     SHIFT_DISPLAY (s->display);
 
-    width = ss->textData->width;
     height = ss->textData->height;
 
     if (shiftGetMultioutputMode (s) == MultioutputModeOneBigSwitcher)
