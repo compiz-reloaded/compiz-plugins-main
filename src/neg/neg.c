a314537f4ecb8093c7679eb14ca447f02eec3d56
Jonathan Michalon <jonathan@michalon.eu>
2019-10-25 15:19:44 +0200

    Add option to negate the window decoration too in neg plugin

--- src/neg/neg.c
+++ src/neg/neg.c
@@ -398,7 +398,7 @@ NEGDrawWindowTexture (CompWindow           *w,
 
     /* only negate window contents; that's the only case
        where w->texture->name == texture->name */
-    if (!nw->isNeg || !((texture->name == w->texture->name) ||
+    if (!nw->isNeg || !((texture->name == w->texture->name) || negGetNegDecorations (w->screen) ||
 		(w->type & CompWindowTypeDesktopMask)) ||
 		!(w->screen->fragmentProgram))
 	{
@@ -410,7 +410,7 @@ NEGDrawWindowTexture (CompWindow           *w,
 		return;
 	}
 
-	function = getNegFragmentFunction (w->screen, texture, w->alpha);
+	function = getNegFragmentFunction (w->screen, texture, negGetNegDecorations (w->screen) ? TRUE : w->alpha);
 	if (function)
 		addFragmentFunction (&fa, function);
 
@@ -471,6 +471,11 @@ NEGScreenOptionChanged (CompScreen       *s,
     case NegScreenOptionPreserveToggled:
 	{
 	    NEGUpdateScreen (s);
+	}
+	break;
+    case NegScreenOptionNegDecorations:
+	{
+	    damageScreen (s);
 	}
 	break;
     default:
@@ -618,6 +623,7 @@ NEGInitScreen (CompPlugin *p,
     negSetToggleScreenByDefaultNotify (s, NEGScreenOptionChanged);
     negSetExcludeMatchNotify (s, NEGScreenOptionChanged);
     negSetPreserveToggledNotify (s, NEGScreenOptionChanged);
+    negSetNegDecorationsNotify (s, NEGScreenOptionChanged);
 
     /* wrap overloaded functions */
     WRAP (ns, s, drawWindowTexture, NEGDrawWindowTexture);
